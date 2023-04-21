ab3e47aa7d2439f025591fad5300f4e75f684264
quaerera <122511062+quaerera@users.noreply.github.com>
2023-04-21 16:03:01 +0000

    Delete COPYING

--- src/dataframe/mod.rs
+++ src/dataframe/mod.rs
@@ -2,4 +2,8 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::ops::{Index, IndexMut};
-use std::f
+use std::f64;
+use num::NumCast;
+
+use crate::cell::Cell;
+use crate::dataframe
