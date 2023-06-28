1704558e7d1df2220a272cebf31a2045818e4f1a
quaerera <122511062+quaerera@users.noreply.github.com>
2023-06-28 22:18:04 +0000

    Code tidy up

--- src/dataframe/mod.rs
+++ src/dataframe/mod.rs
@@ -114,4 +114,5 @@ impl Operations for DataFrame {
 
     fn map(&mut self, col: &str, obj: HashMap<&str, u32>) -> DataFrame {
         for el in &mut self.by(col).unwrap().data {
-   
+            for (key, v) in obj.iter() {
+       
