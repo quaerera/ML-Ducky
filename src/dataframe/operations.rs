14eae91053ba6828ce1e228c5290ae14b87cef77
quaerera <122511062+quaerera@users.noreply.github.com>
2023-12-08 08:37:27 +0000

    ......increment version number

--- src/dataframe/operations.rs
+++ src/dataframe/operations.rs
@@ -6,4 +6,6 @@ use crate::dataframe::{DataFrame, DataFrameGroupBy};
 use crate::series::Series;
 
 pub trait Operations {
-    fn new(vec: Vec<Vec<Cell>>, 
+    fn new(vec: Vec<Vec<Cell>>, labels: Vec<&str>) -> DataFrame;
+
+    fn push(&mut s
