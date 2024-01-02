aabe491ef84af8f99e9b40f60b055d87fdcf8f4d
quaerera <122511062+quaerera@users.noreply.github.com>
2024-01-02 14:24:13 +0000

    Fixed #20

--- src/dataframe/science.rs
+++ src/dataframe/science.rs
@@ -3,4 +3,7 @@ use std::iter::Product;
 use num::{PrimInt, Signed};
 
 use crate::dataframe::DataFrame;
-use crate::algebraic::matrix
+use crate::algebraic::matrix::Matrix;
+use crate::cell::Cell;
+
+pub trait Scie
