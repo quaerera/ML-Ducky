6270d4487af35e78dab895361546a332d1306ec0
quaerera <122511062+quaerera@users.noreply.github.com>
2024-01-07 15:05:52 +0000

    add plugins

--- src/dataframe/science.rs
+++ src/dataframe/science.rs
@@ -10,4 +10,5 @@ pub trait Science {
     /// One hot encoding - Convert string values to binary value
     fn get_dummies(&mut self, label: &str) -> DataFrame;
 
-    fn from_vec<T>(vec: Vec<Vec<T>>, labels: Vec<&str>) -> Da
+    fn from_vec<T>(vec: Vec<Vec<T>>, labels: Vec<&str>) -> DataFrame
+        where T: PrimInt + Signed +
