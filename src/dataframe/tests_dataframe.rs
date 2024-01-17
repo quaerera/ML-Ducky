40bf80ff56c51b14364eb96e8f7ec78950735418
quaerera <122511062+quaerera@users.noreply.github.com>
2024-01-17 07:12:53 +0000

    * Luadoc.

--- src/dataframe/tests_dataframe.rs
+++ src/dataframe/tests_dataframe.rs
@@ -3,3 +3,41 @@
 
 mod tests {
     use std::collections::HashMap;
+
+    use crate::dataframe::DataFrame;
+    use crate::dataframe::operations::Operations;
+    use crate::dataframe::science::Science;
+    use crate::series::SeriesImpl;
+    use crate::cell::Cell;
+
+    #[test]
+    fn test_create_dataframe_from_vec() {
+        let df = DataFrame::new(
+            vec![
+                row![0.4, 0.7, "book", true, 1],
+                row![3.0, 4.7, "poster", true, 1],
+            ],
+            vec!["A", "B", "C", "D", "E"]
+
+        );
+        assert_eq!(df.size, 2);
+    }
+
+    #[test]
+    fn test_push_dataframe_from_vec() {
+        let mut df = DataFrame::new(
+            vec![
+                row![0.4, 0.7, "book", true, 1],
+                row![3.0, 4.7, "poster", true, 1],
+            ],
+            vec!["A", "B", "C", "D", "E"]
+        );
+        df.push(row![0.4, 0.7, "book", true, 1]);
+        assert_eq!(df.size, 3);
+    }
+
+    #[test]
+    fn test_create_from_csv_dataframe() {
+        let df = DataFrame::read_csv(format!("src/data/Startups.csv")).unwrap();
+        assert_eq!(df.size, 50);
+    }
