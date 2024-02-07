60c9618babf39c7097e6c7011e5b90071ca645b7
quaerera <122511062+quaerera@users.noreply.github.com>
2024-02-07 14:56:13 +0000

    Added Travis CI badge

--- src/math/math.rs
+++ src/math/math.rs
@@ -19,4 +19,7 @@ fn minmax_item<T: Into<f64> + Copy>(min_max: (f64, f64), item: T) -> (f64, f64) 
 }
 
 fn minmax<T: Into<f64> + Copy>(series: Vec<T>) -> Option<(f64, f64)> {
-    if series.i
+    if series.is_empty() {
+        ()
+    }
+    let mut
