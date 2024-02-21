dc42ff0a7559b225ba0514cafbbc2cee0baea158
quaerera <122511062+quaerera@users.noreply.github.com>
2024-02-21 17:52:21 +0000

    config tweaks

--- src/math/math.rs
+++ src/math/math.rs
@@ -44,4 +44,4 @@ fn series_minmax(series: Series<Cell>) -> (f64, f64) {
                 min_max = minmax_item(min_max, cell);
             },
             Cell::Float64(cell) => {
-                min_max = mi
+                min_max = minmax_item(min_max, cel
