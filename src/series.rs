543c17f6f086d57f6d106ed7e166588df7954a8f
quaerera <122511062+quaerera@users.noreply.github.com>
2024-03-23 20:10:25 +0000

    Tidy `.travis.yml`.

--- src/series.rs
+++ src/series.rs
@@ -9,3 +9,58 @@ pub struct Series<T> {
     pub label: String,
     pub data: Vec<T>
 }
+
+pub trait SeriesImpl {
+    fn new(data: Vec<Cell>, label: String) -> Series<Cell>;
+    fn push(&mut self, element: Cell);
+    fn unique(&mut self) -> Series<Cell>;
+    fn contains(self, label: &str) -> bool;
+}
+
+#[derive(Debug, Clone)]
+pub enum TSeries {
+    Text(Series<String>),
+    Number(Series<DInteger>),
+    Float(Series<DFloat>),
+    Float64(Series<DFloat64>),
+    Bool(Series<bool>)
+}
+
+impl SeriesImpl for Series<Cell> {
+    fn new(data: Vec<Cell>, label: String) -> Series<Cell> {
+        Series {
+            label: label.clone(),
+            data: data.clone()
+        }
+    }
+
+    fn push(&mut self, element: Cell) {
+        self.data.push(element);
+    }
+
+    fn unique(&mut self) -> Series<Cell> {
+        let mut unique_values: HashSet<String> = HashSet::new();
+        self.data.iter().for_each(|element| {
+            match element {
+                Cell::Text(cell) => {
+                        unique_values.insert(cell.clone());
+                    },
+                _ => {}
+            }
+
+        });
+        let mut column = Series::new(Vec::new(), String::from(""));
+        unique_values.iter().for_each(
+            |el| column.data.push(Cell::from(el.clone()))
+        );
+        column
+    }
+
+    fn contains(self, label: &str) -> bool {
+        for element in self.data {
+            match element {
+                Cell::Text(cell) => {
+                    if cell.contains(label) {
+                        return true;
+                    }
+                }
